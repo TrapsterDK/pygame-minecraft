24146c0342ded29c6b4564df083a06d7dbc5cc7f
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-11-01 17:30:03 +0100

    Enabled depth test and smoothing

--- scripts/cube.py
+++ scripts/cube.py
@@ -4,17 +4,22 @@ from OpenGL.GL import *
 #https://learnopengl.com/Getting-started/Camera
 cubeVertices = ((1,1,1),(1,1,-1),(1,-1,-1),(1,-1,1),(-1,1,1),(-1,-1,-1),(-1,-1,1),(-1, 1,-1))
 cubeQuads = ((0,3,6,4),(2,5,6,3),(1,2,5,7),(1,0,4,7),(7,4,6,5),(2,3,0,1))
+side_color = ((1,0,0), (0,1,0), (0,0,1), (1,1,0), (1,0,1), (0,1,1))
 
 def solidCube():
     glBegin(GL_QUADS)
-    for cubeQuad in cubeQuads:
+    for color, cubeQuad in zip(side_color, cubeQuads):
+        glColor3fv(color)
         for cubeVertex in cubeQuad:
             glVertex3fv(cubeVertices[cubeVertex])
     glEnd()
 
 # Statup runs once at the start of the game
 def startup():
-    pass
+    # enable depth test
+    glEnable(GL_DEPTH_TEST)
+    # enable smooth shading
+    glShadeModel(GL_SMOOTH)
 
 # Pre update runs before event handling and update
 def pre_update():
