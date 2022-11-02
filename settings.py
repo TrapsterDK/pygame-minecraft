b722a3b9c30fdb9eaab8e21cdc28b803a71a53c1
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-11-02 22:02:02 +0100

    removed useless settings

--- settings.py
+++ settings.py
@@ -1,9 +1,3 @@
-from pygame import RESIZABLE, OPENGL
-
-# Console Debugging
-DEBUG = True
-LOGGING_FILE = "log.txt"
-
 # Folder where scripts are stored
 SCRIPTS_FOLDER = 'scripts'
 
@@ -16,11 +10,5 @@ SCREEN_SIZE_DEFAULT = (800, 600)
 # Default fps, set to 0 to disable fps limit
 FPS_MAX_DEFAULT = 60
 
-# Flags for pygame.display.set_mode, already includes DOUBLEBUF
-FLAGS = RESIZABLE | OPENGL
-
-# Weather to display fps in left top corner
-FPS_COUNTER = True
-
 # Default clear color
-CLEAR_COLOR = (173, 216, 230) #light blue
+CLEAR_COLOR = (0, 0, 0) #(173, 216, 230) #light blue
