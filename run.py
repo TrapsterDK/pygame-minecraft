6cd7af2b9f25edeb971a5a59070f67b073a4765b
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-11-02 22:01:31 +0100

    run file to work with new engine

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-from importer import load_functions
-from gameengine import start_game, set_callbacks
-
-def run():
-    functions = load_functions()
-    set_callbacks(**functions)
-    start_game()
+from gameengine import start_game
 
 if __name__ == "__main__":
-    run()
+    start_game()
