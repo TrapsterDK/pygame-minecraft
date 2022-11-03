c207f7cf2b39abad0c0aa205ffd85ea697ac21b6
TrapsterDK <martin.kuntz.jacobsen@gmail.com>
2022-11-03 12:38:58 +0100

    mutiple textures rendered together

--- scripts/textured-triangle.py
+++ scripts/textured-triangle.py
@@ -1,7 +1,7 @@
 from gameengine import get_gl_context, load_texture, get_shader 
 from array import array
 
-IGNORE_MODULE = 1
+#IGNORE_MODULE = 1
 
 vertices = [ 
     -0.5, -0.5, 0.0, 0.0, # bottom left,  texture coordinate
@@ -17,9 +17,17 @@ def startup():
 
     program = get_shader("textured-triangle")
 
+    program['tex'] = 0
+    program['tex2'] = 1
+
     texture = load_texture("test.png")
+    texture_2 = load_texture("test2.png")
+    
     texture.filter = (ctx.NEAREST, ctx.NEAREST) # blocky instead of blurry
-    texture.use()
+    texture_2.filter = (ctx.NEAREST, ctx.NEAREST) # blocky instead of blurry
+    
+    texture.use(location=0)
+    texture_2.use(location=1)
 
     vbo = ctx.buffer(array('f', vertices))
     
--- shaders/textured-triangle.fs
+++ shaders/textured-triangle.fs
@@ -4,8 +4,13 @@ out vec4 f_color;
 in vec2 v_tex_coord;
 
 uniform sampler2D tex;
+uniform sampler2D tex2;
 
 void main()
 {
-    f_color = texture(tex, v_tex_coord);
+    if (texture(tex2, v_tex_coord).a == 0.0 ){
+        f_color = texture(tex, v_tex_coord);
+    }else{
+        f_color = mix(texture(tex, v_tex_coord), texture(tex2, v_tex_coord), 0.8);
+    }
 }
